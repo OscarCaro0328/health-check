3173c7111253232b57fa16c667097e09fd2e55db
OscarCaro0328 <oscar.ricardo.caro@gmail.com>
2025-03-04 11:09:17 -0500

    API getPreview working

---
+++ src/app/api/healthCheck/getPreview/route.js
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server"; // App router
+//import axios from "axios";
+
+export async function GET(request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const nsn = searchParams.get('nsn');
+
+    if (!nsn || isNaN(nsn)) {
+      return NextResponse.json({ error: 'Invalid nsn provided' }, { status: 400 });
+    }
+
+    const imageUrl = `https://us.dunkindonuts.switchboardcms.com/device/${nsn}-107/screenshot`;
+    console.log(imageUrl);
+
+    const response = await fetch(imageUrl);
+
+    if (!response.ok) {
+      return NextResponse.json({ error: `Image not found (status ${response.status})` }, { status: response.status });
+    }
+
+    const arrayBuffer = await response.arrayBuffer();
+
+    const headers = {
+      'Content-Type': response.headers.get('content-type'),
+    };
+
+    return new NextResponse(arrayBuffer, { status: 200, headers });
+
+  } catch (error) {
+    console.error('Error fetching image:', error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
