483aebc4d823c59f8efbdeb8249de1b02aad75ae
OscarCaro0328 <oscar.ricardo.caro@gmail.com>
2025-03-07 10:40:26 -0500

    Active and getAllPreviews updated to work with bits of 6 nsns

--- src/app/api/healthCheck/isActive/route.js
+++ src/app/api/healthCheck/isActive/route.js
@@ -1,13 +1,18 @@
 import { NextResponse } from "next/server"; // App router
-import { nsns } from "./data";
+//import { nsns } from "./data";
+import { getNsnsForPage } from "./data.js";
 
 const domain_dd = "https://us.dunkindonuts.switchboardcms.com";
 const domain_mcd_uat = "https://us-mcd.uat.switchboardcms.com/";
 const domain_arbys = "https://us-arbys.switchboardcms.com";
 //const nsns = ["353155", "306321", "301677","1111101"];
 
-export async function GET() {
+export async function GET(request) {
   try {
+        const { searchParams } = new URL(request.url);
+        const page = searchParams.get('page');
+        let nsns = getNsnsForPage(page);
+        console.log(nsns)
     // Check URL availability for all NSNs in parallel
     const results = await Promise.allSettled(
       nsns.map(async (nsn) => {
@@ -15,6 +20,7 @@ export async function GET() {
 
         try {
           const response = await fetch(imageUrl, { method: "HEAD" });
+          //console.log(response);
           return {
             nsn,
             status: response.ok
@@ -29,6 +35,12 @@ export async function GET() {
     //console.log(results);
     // Format the final response
     const formattedResults = results.map((result) => result.value);
+
+    const nsn_available = formattedResults
+      .filter((item) => item.status === "available")
+      .map((item) => item.nsn);
+    console.log(nsn_available);
+
     return NextResponse.json({ urls: formattedResults });
   } catch (error) {
     console.error("Error checking URLs:", error);
